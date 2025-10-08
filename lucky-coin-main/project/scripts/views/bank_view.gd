b11b652458b8d00f835bc7dd5fc82a7a71e4a4ef
zkz <1399443067@qq.com>
2025-10-08 13:33:19 +0800

    删除多余的文件夹

--- lucky-coin-main/project/scripts/views/bank_view.gd
+++
@@ -1,8 +0,0 @@
-extends "res://project/scripts/views/base_view.gd" 
-
-# @onready var bank_ui: Control = $BankUI # 未来你会在这里引用银行界面
-
-func _ready():
-	super._ready()
-	print("Bank View Loaded") 
-	# 在这里添加银行特有的初始化代码
--- lucky-coin-main/project/scripts/views/store_view.gd
+++
@@ -1,9 +0,0 @@
-extends "res://project/scripts/views/base_view.gd" 
-
-# @onready var store_button: Button = $StoreButton # 未来你会在这里引用商店按钮
-
-func _ready():
-	# 确保调用父类的 _ready，让背景设置生效
-	super._ready()
-	print("Store View Loaded") 
-	# 在这里添加商店特有的初始化代码
